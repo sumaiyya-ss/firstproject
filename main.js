d386fadce9f950efa04d39804f9a154c10bc2190
sumaiyya-ss <sumaiyyabawany@gmail.com>
2024-12-27 07:32:36 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+alert("welcome to javascript");
+prompt("what is your name?");
+var firstname =prompt("enter your first name") ;
+var secondname =prompt("enter your second name");
+var fullname = (firstname + secondname);
+alert("full name =");
+var age = prompt("enter your age");
+var yearofbirth = prompt(" enter your yearofbirth");
+var currentyear =prompt(" enter current year ") ;
+var yourage =(currentyear-yearofbirth);
+alert ("your age =");
+var age=prompt("enter your age");
+var friendage=prompt("enter your friend's age");
+var sumofages=(age-friendage);
+alert("sum of ages =");
+var firstnum=prompt("enter first number");
+var secondnum=prompt("enter second number");
+var sum=(firstnum+secondnum);
+alert("sum of numbers =");
+var difference=(firstnum-secondnum);
+alert("difference:");
+var product=(firstnum*secondnum);
+alert("product =");
+var firstvalue=4
+var secondvalue=10
+alert("firstvalue=4");
+alert("second value=10")
+var percentage=(firstvalue/secondvalue*100);
+alert ("result in percentage=");
+
